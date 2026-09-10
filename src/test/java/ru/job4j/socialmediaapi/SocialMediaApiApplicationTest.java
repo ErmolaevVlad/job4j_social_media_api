09dd2dc8b5631bebf9073c15b8345db2f9954066
ErmolaevVlad <ermolaev.vlad.0109@gmail.com>
2026-09-10 23:13:31 +0700

    Testcontainers

--- src/test/java/ru/job4j/socialmediaapi/SocialMediaApiApplicationTest.java
+++ src/test/java/ru/job4j/socialmediaapi/SocialMediaApiApplicationTest.java
@@ -2,9 +2,14 @@ package ru.job4j.socialmediaapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -12,7 +17,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class SocialMediaApplicationTests {
+@Testcontainers
+class SocialMediaApiApplicationTest {
+    @Container
+    @ServiceConnection
+    private static final PostgreSQLContainer<?> POSTGRES_CONTAINER = new PostgreSQLContainer<>("postgres:17");
+
     @Autowired
     private MockMvc mockMvc;
 
